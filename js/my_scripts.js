22cec19e1290449c4c6ff2e624b3eeaabe81cf0a
Phoenix-Pool <contact@phoenix-adapool.com>
2021-01-31 21:13:24 -0700

    Website Design Update

--- js/my_scripts.js
+++ js/my_scripts.js
@@ -1,1 +1,10 @@
 
+function toggleNav() {
+    if (document.getElementById("sidebar").style.width != "400px") {
+        document.getElementById("sidebar").style.visibility = "visible";
+        document.getElementById("sidebar").style.width = "400px";
+    } else {
+        document.getElementById("sidebar").style.width = "0px";
+        document.getElementById("sidebar").style.visibility = "hidden";
+    }
+}
